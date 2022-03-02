07d7a32183071235628cb6f0df01d9e9592d2aee
Aissa-Genius <81246875+Aissa-Genius@users.noreply.github.com>
2022-03-02 16:06:09 -0500

    change path

--- config/snappy.php
+++ config/snappy.php
@@ -35,8 +35,8 @@ return [
 
     'pdf' => [
         'enabled' => true,
-         //'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
-        'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
+          'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+       // 'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
         'timeout' => false,
         'options' => [
 
